47023492d225ce753ea59e938d875dbcc4d9c419
gutoo01 <gutrindadelive@gmail.com>
2022-09-30 20:51:28 -0300

    fix

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import car from '../../assets/car1.png'
+import car from '../../../public/assets/car1.png'
 
 export default function Product(props) {
   return (
     <>
-      <div className="bg-gray100 box w-80 h-72 rounded-xl shadow-md shadow-black-500/50 m-20">
+      <div className="bg-gray100 box w-80 h-72 rounded-xl shadow-md shadow-black-500/50 m-8">
         <Image src={car} className="rounded-t-xl" />
         <div className='px-2' >
           <strong className='font-sans text-lg'>Mitsubishi Eclipse Cross HPE-S</strong>
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,11 +2,20 @@ import { Navbar } from "../../components/Navbar";
 import Product from "../../components/Product";
 
 
+
 export default function Dashboard(props) {
   return (
     <>
       <Navbar />
-      <Product />
+      <div className="grid grid-cols-4">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
     </>
   )
 }
+
